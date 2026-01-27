965a57129c0af6125d6047debbe2bf61765434fe
Seftian Alung <naitfess@gmail.com>
2026-01-27 15:45:17 +0700

    update logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,10 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <div>
+                        <img style="width: 100px" class="logo-front img-fluid" src="{{ asset('logo/logo_muarabungo.PNG') }}" alt="logo-muara-bungo">
+                    </div>
+                    {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
                 </a>
             </div>
 
